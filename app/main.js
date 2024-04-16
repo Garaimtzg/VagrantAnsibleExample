d02aaa98506943198cad5f5bc0456a50d894248f
Garai Martínez de Santos Gómez <cooperacion-110885@tecnalia.com>
2024-04-16 09:21:05 +0200

    Ejemplo finalizado y errores corregidos

--- app/main.js
+++ app/main.js
@@ -6,10 +6,21 @@ const app = express();
 
 // Definimos una ruta para la página principal
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+    res.send(`
+    <html lang="">
+      <head>
+        <title>Mi Página</title>
+      </head>
+      <body>
+        <h1>¡Hola, mundo!</h1>
+        <p>Bienvenido a mi página.</p>
+      </body>
+    </html>
+  `);
 });
 
-// Iniciamos el servidor en el puerto 3000
-app.listen(3000, () => {
-  console.log('Aplicación escuchando en el puerto 3000');
+// Iniciamos el servidor en el puerto 3001
+app.listen(3001, () => {
+    console.log('Aplicación escuchando en el puerto 3001, la ip de la máquina es 192.168.' +
+        '56.12, usa curl http://192.168.56.12:3001\n para ver que funciona o buscalo en el navegador web');
 });
